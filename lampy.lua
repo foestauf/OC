ec96fda600c97745ce9241a4cc2ac5af230e0ca3
Foestauf <46908845+foestauf@users.noreply.github.com>
2019-08-01 21:30:27 -0500

    Update lampy.lua

--- lampy.lua
+++ lampy.lua
@@ -2,6 +2,9 @@ print("Say Hello to Lampy!")
 while true do
     local lightlevel = component.redstone.getInput(sides.north)
     term.write(lightlevel)
-    if lightlevel > 6 then component.redstone.setOutput(sides.west, 15) else component.redstone.setOutput(sides.west, 0)
+    if lightlevel > 6 then
+      component.redstone.setOutput(sides.west, 15)
+    else component.redstone.setOutput(sides.west, 0)
+    end
 end
 
