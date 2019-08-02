9b1ded4f101822133b882178a329a581171c8649
Foestauf <46908845+foestauf@users.noreply.github.com>
2019-08-01 21:47:45 -0500

    Update lampy.lua

--- lampy.lua
+++ lampy.lua
@@ -1,10 +1,11 @@
 print("Say Hello to Lampy!")
 local component = require("component")
 local term = require("term")
+local sides = require("sides")
 while true do
     local lightlevel = component.redstone.getInput(sides.north)
     term.write(lightlevel)
-    if lightlevel > 6 then
+    if lightlevel < 8 then
       component.redstone.setOutput(sides.west, 15)
     else component.redstone.setOutput(sides.west, 0)
     end
