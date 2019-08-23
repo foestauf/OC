8189aca97e3667b6fae52c076eb270705b8a7266
foestauf <46908845+foestauf@users.noreply.github.com>
2019-08-22 22:56:14 -0400

    Update lampy.lua

--- lampy.lua
+++ lampy.lua
@@ -2,6 +2,7 @@ print("Say Hello to Lampy!")
 local component = require("component")
 local term = require("term")
 local sides = require("sides")
+local colors = require("colors")
 while true do
     local lightlevel = component.redstone.getbundledInput(sides.west,colors.pink)
     term.write(lightlevel)
