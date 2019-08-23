0e4d6baea7883d5bef9d38b378461770417b1657
foestauf <46908845+foestauf@users.noreply.github.com>
2019-08-22 23:04:06 -0400

    Update lampy.lua

--- lampy.lua
+++ lampy.lua
@@ -3,13 +3,12 @@ local component = require("component")
 local term = require("term")
 local sides = require("sides")
 local colors = require("colors")
-local redstone = require("redstone")
 while true do
     local lightlevel = component.redstone.getbundledInput(sides.west,colors.pink)
     term.write(lightlevel)
     if lightlevel < 8 then
-      component.redstone.setbundledOutput(sides.east,colors.white, 255)
-    else component.redstone.setbundledOutput(sides.east,colors.white, 0)
+      component.redstone.setBundledOutput(sides.east,colors.white, 255)
+    else component.redstone.setBundledOutput(sides.east,colors.white, 0)
     end
   os.sleep(5)
 end
