fd17923c91412f1d3a23c456ea6405e46e99b0a3
foestauf <46908845+foestauf@users.noreply.github.com>
2019-08-22 23:44:29 -0400

    Update lampy.lua

--- lampy.lua
+++ lampy.lua
@@ -4,10 +4,13 @@ local term = require("term")
 local sides = require("sides")
 local colors = require("colors")
 while true do
-    local lightlevel = component.redstone.getbundledInput(sides.east,colors.pink)
+    local lightlevel = component.redstone.getBundledInput(sides.east,colors.pink)
+	local raining = component.redstone.getBundledInput(sides.east,colors.cyan)
     term.write(lightlevel)
     if lightlevel < 150 then
       component.redstone.setBundledOutput(sides.east,colors.white, 255)
+	elseif raining > 100 then
+	component.redstone.setBundledOutput(sides.east,colors.white, 255)
     else component.redstone.setBundledOutput(sides.east,colors.white, 0)
     end
   os.sleep(5)
