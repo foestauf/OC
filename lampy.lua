894d9ee56c1ad4102e31a1a27c6c121e8a491b41
foestauf <46908845+foestauf@users.noreply.github.com>
2019-08-24 17:10:42 -0400

    Update lampy.lua

--- lampy.lua
+++ lampy.lua
@@ -3,32 +3,58 @@ local term = require("term")
 local sides = require("sides")
 local colors = require("colors")
 local gpu = component.gpu
+local redstone = component.redstone
+w, h = component.gpu.getResolution()
 
-
-local function status(x,y)
-    term.clear()
-	local w, h = gpu.getResolution()
-	gpu.setBackground(0x316E1F) -- Green Background
+function status()
+	-- term.clear()
+	local rain = " "
+	-- gpu.setBackground(0xB9B9B9) -- Gray Background
 	gpu.fill(1,1,w,h, " ")
-	io.write("Current light level: ",x, "\n")
-	if y > 100 then y = "Yes"
-        else y = "No"
+	lights()
+	io.write("Current light level: ",lightlevel, "\n")
+	if raining > 100 then rain = "Yes"
+        else rain = "No"
 	end
 	io.write("Is it raining?: ")
 	gpu.setForeground(0xFF0000) -- Red
-	io.write(y)
+	io.write(rain)
 	gpu.setForeground(0xFFFFFF) -- White
-	end
-while true do
-    local lightlevel = component.redstone.getBundledInput(sides.east,colors.pink)
-	local raining = component.redstone.getBundledInput(sides.east,colors.cyan)
-	status(lightlevel,raining)
-    if lightlevel < 150 then
-      component.redstone.setBundledOutput(sides.east,colors.white, 255)
-	elseif raining > 100 then
-	component.redstone.setBundledOutput(sides.east,colors.white, 255)
-    else component.redstone.setBundledOutput(sides.east,colors.white, 0)
-    end
-  os.sleep(5)
+	-- drawButton(25,25,"Goof")
+	updateRain(rain)
 end
 
+function updateRain(x)
+	local startX = w/5
+	local startY = h/5
+	term.setCursor(startX,startY)
+	term.clearLine()
+	-- term.setCusor(startX,startY)
+	io.write("New Rain Status Yo: ", x)
+end
+
+function lights()
+    lightlevel = redstone.getBundledInput(sides.east,colors.pink)
+	raining = redstone.getBundledInput(sides.east,colors.cyan)
+    if lightlevel < 150 then
+      redstone.setBundledOutput(sides.east,colors.white, 255)
+	elseif raining > 100 then
+	redstone.setBundledOutput(sides.east,colors.white, 255)
+    else redstone.setBundledOutput(sides.east,colors.white, 0)
+    end
+end
+
+function drawButton(x,y,name)
+	gpu.setBackground(0x45EF11)
+	gpu.fill(x-2,y-2,8,5, " ")
+	term.setCursor(x,y)
+	gpu.setForeground(0xFF0000)
+	io.write(name)
+	gpu.setBackground(0x000000)
+	gpu.setForeground(0xFFFFFF)
+end
+
+while true do
+    status()
+    os.sleep(5)
+end
