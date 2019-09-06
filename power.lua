991b88bd5542cc10ba090c03678b8338a4c944b3
acciobroom <52329590+acciobroom@users.noreply.github.com>
2019-09-05 23:40:32 -0400

    Update power.lua

--- power.lua
+++ power.lua
@@ -3,12 +3,56 @@ local fusion = component.nc_fusion_reactor
 local complete = fusion.isComplete()
 local gpu = component.gpu
 
-if complete == true then
-	fusion.activate()
-else
-	why = fusion.getProblem()
-	io.write("I am not complete because ", why)
+function buildStatus()
+	local complete = fusion.isComplete()
+	return complete
 end
+
+function buildInfo()
+	bool = buildStatus()
+	if bool == true then
+		fusion.activate()
+	else
+		why = fusion.getProblem()
+		io.write("I am not complete because ", why)
+end
+
+function currentTemp()
+	return temp = fusion.getTemperature()
+end
+	
+function currentEnergy()
+	return energy = fusion.getEnergyStored()
+end
+
+function overheat()
+	temp = currentTemp()
+	maxtemp = fusion.getMaxTemperature()
+	overtemp = 0.75*maxtemp
+	overheatStatus = false
+	if currentTemp > overtemp then
+		io.write("Overheatiing: Current Temp = ", temp)
+		return overheatStatus = true
+		fusion.deactivate()
+	else
+		return overheatStatus = false
+end
+		
+function energyfull()	
+	energy = currentEnergy()
+	maxenergy = fusion.getMaxEnergyStored()
+	overenergy = 0.75*maxenergy
+	energyStatus = false
+	if currentTemp > overenergy then
+		io.write("Current Power Storagy = ", energy)
+		return energyStatus = true
+		fusion.deactivate()
+	else
+		return energyStatus = false
+end	
+		
+	
+	
 -- AM I COMPLETE
 
 -- YES/NO WHY NOT
