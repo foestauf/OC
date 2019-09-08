c2584dbde63ff5104fcbdad2986cf33ea5d7a5ec
foestauf <46908845+foestauf@users.noreply.github.com>
2019-09-08 01:55:39 -0400

    Update power.lua

--- power.lua
+++ power.lua
@@ -1,7 +1,10 @@
 local component = require("component")
 local fusion = component.nc_fusion_reactor
 local gpu = component.gpu
-
+local running = true
+local term = require("term")
+local energy = 0
+local status = Normal
 
 -- Status Functions
 function buildStatus()
@@ -16,18 +19,68 @@ function currentEnergy()
 	return fusion.getEnergyStored()
 end
 
+local function normalStatus()
+	term.clear()
+	io.write("TCRM Reactor Status\n")
+	-- io.write("Status")
+	io.write("\n Current Energy Level: ", energy)
+	if temp > 1000000 then
+		temp = temp/1000000
+		tempMeas = "MK"
+	else
+		temp = temp/1000
+		tempMeas = "kK"
+	end
+	io.write("\n Temperature: ", math.floor(temp))
+	io.write("\n Current RF/t: ", math.floor(fusion.getReactorProcessPower()))
+	io.write(tempMeas)
+	local efficiency = fusion.getEfficiency()
+	efficiency = math.floor(efficiency)
+	io.write("\n Current Efficiency: ", efficiency, "%")
+end
+
 -- Check Current Status --
 function buildInfo()
 	bool = buildStatus()
 	
-	if bool == true then
+	if bool == false then
 		why = fusion.getProblem()
-		io.write("I am not complete because ", why)
+		exit_msg(why)
 	end
 	
 	return bool
 end
 
+
+function safety()
+	overheat()
+	energyfull()
+	io.write("\n Safeguardian Enabled\n")
+	if energyStatus == true then
+		term.write("\nInternal buffer is full\n")
+		os.sleep(10)
+		energyfull()
+		if energyStatus == false then
+		restart()
+		end
+	end
+	if overheat == true then
+		io.write("OVERHEATED!")
+		os.sleep(10)
+		overheat()
+		if overheatStatus == false then
+			restart()
+		end
+	end
+end
+
+function restart()
+	io.write("Attempting to restart reactor\n")
+	if buildInfo() == true and overheat() == false and energyfull() == false then
+		fusion.activate()
+		status = Normal
+	end
+end
 
 function overheat()
 	temp = currentTemp()
@@ -49,11 +102,11 @@ function energyfull()
 	energy = currentEnergy()
 	local maxenergy = fusion.getMaxEnergyStored()
 	almostFull = 0.75*maxenergy
-	energyStatus = false
+	energyStatus = false	
 	
 	if energy > almostFull  then
-		io.write("Current Power Storage = ", energy)
-		energyStatus = false
+		-- io.write("Current Power Storage = ", energy)
+		energyStatus = true
 		fusion.deactivate()
 	end
 	
@@ -64,16 +117,41 @@ end
 function start()
 	if buildInfo() == true and overheat() == false and energyfull() == false then
 		fusion.activate()
-	elseif overheat() == true then 
+		status = Normal
+	elseif overheat() == true then
+		term.clear()
 		io.write("Overheat Condition")
-	elseif energyfull() == true then 
+	elseif energyfull() == true then
+		term.clear()	
 		io.write("Internal Buffer Full")
-	elseif buildInfo() == false then 
+	elseif buildInfo() == false then
+		term.clear()	
 		io.write("Not built properly?")
 	end
 end
 	
-	
+
+-- Exit Message
+function exit_msg(msg)
+  term.clear()
+  fusion.deactivate()
+  term.write("TCRM Exiting!\n")
+  print(msg)
+  os.exit()
+end
+-- Start Reactor
+start()
+
+-- Main Loop
+while running do
+	term.clear()
+	if status == Normal then
+		normalStatus()
+		safety()
+	end
+	os.sleep(1)
+end
+
 -- AM I COMPLETE
 
 -- YES/NO WHY NOT
